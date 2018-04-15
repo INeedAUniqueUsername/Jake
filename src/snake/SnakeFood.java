ce2547430be25b1a64c5b9d752033c202eb396e7
INeedAUniqueUsername <achen115@ucr.edu>
2018-04-14 23:05:33 -0700

    implement SnakeFood

---
+++ src/snake/SnakeFood.java
@@ -0,0 +1,31 @@
+package snake;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+
+public class SnakeFood extends GameObject {
+	int width = 16;
+	int height = 16;
+	public static final Image sprite = SnakeSprites.load(new File(SnakeSprites.DIR + "food.png"));
+	public SnakeFood() {
+		this(0, 0);
+	}
+	public SnakeFood(int x, int y) {
+		setPos(x, y);
+	}
+	public void draw(Graphics window) {
+/*		window.setColor(Color.yellow);
+		window.fillRect(xPos, yPos, 16, 16);*/
+		window.drawImage(sprite, posX, posY, width, height, null);
+	}
+	public void onCollision(GameObject o) {}
+	@Override
+	public void update(int time) {
+		// TODO Auto-generated method stub
+		
+	}
+}
