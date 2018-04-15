373f2aafc2986beb2aed2950adffb508a80d1f06
INeedAUniqueUsername <achen115@ucr.edu>
2018-04-14 23:04:46 -0700

    standard GameObject functions

--- src/snake/GameObject.java
+++ src/snake/GameObject.java
@@ -1,22 +1,27 @@
 package snake;
+import java.awt.Graphics;
 import java.awt.Point;
 
-public class GameObject {
-	boolean active = true;
-	int posX = 0;
-	int posY = 0;
-	
+public abstract class GameObject {
+	protected boolean active = true;
+	protected int posX = 0, posY = 0;
 	public int getPosX() {
 		return posX;
 	}
 	public void setPosX(int posX) {
 		this.posX = posX;
 	}
+	public void incPosX(int posX) {
+		this.posX += posX;
+	}
 	public int getPosY() {
 		return posY;
 	}
 	public void setPosY(int posY) {
 		this.posY = posY;
+	}
+	public void incPosY(int posY) {
+		this.posY += posY;
 	}
 	public Point getPos() {
 		return new Point(posX, posY);
@@ -25,13 +30,17 @@ public class GameObject {
 		setPosX(x);
 		setPosY(y);
 	}
+	public void setPos(Point pos) {
+		setPosX(pos.x);
+		setPosY(pos.y);
+	}
 	public void setActive(boolean a) {
 		active = a;
 	}
 	public boolean getActive() {
 		return active;
 	}
-	public void onCollision(GameObject o) {
-		
-	}
+	public abstract void update(int timeInterval);
+	public abstract void draw(Graphics g);
+	public abstract void onCollision(GameObject o);
 }
