c2ee90abdc3c87d2033863dc30e213a92497c44c
INeedAUniqueUsername <achen115@ucr.edu>
2018-04-14 23:06:35 -0700

    rewrite GameArea and advanced collision

--- src/snake/GameArea.java
+++ src/snake/GameArea.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 import javax.sound.sampled.AudioInputStream;
@@ -20,6 +22,10 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import snake.SnakeHead.Ability;
+import snake.SnakeHead.Direction;
+import snake.SnakeSprites.BodySprites;
 
 public class GameArea extends JPanel implements KeyListener, Runnable {
 
@@ -31,37 +37,49 @@ public class GameArea extends JPanel implements KeyListener, Runnable {
 	int timeInterval = 25;
 	private int gameWidth;
 	private int gameHeight;
-	ArrayList<GameObject> objects;
+	private ArrayList<GameObject> objects;
+	private ArrayList<GameObject> created;
+	private SnakeHeadPlayer player;
 	
 	private final Font FONT;
 	
 /*	Food[] foodzlist;*/
 
-	public static int nextInt(int max)
-	{
+	public static int nextInt(int max) {
 		return (int)(Math.random()*max);
 	}
 	
-	public int randomX()
-	{
+	public int randomX() {
 		return nextInt(gameWidth/16)*16;
 	}
-	public int randomY()
-	{
+	public int randomY() {
 		return nextInt(gameHeight/16)*16;
 	}
+	public int round(int n, int interval) {
+		return interval * (n/interval);
+	}
 	
-	public void reset()
-	{
+	public void reset() {
 		timer = 0;
 		score = 0;
+		player = new SnakeHeadPlayer(this);
+		player.setPos(new Point(round(gameWidth/2, 16), round(gameHeight/2, 16)));
+		objects = new ArrayList<>();
+		objects.add(player);
+		objects.add(createFood());
+		objects.add(createEnemy());
+		created = new ArrayList<>();
+	}
+	public SnakeFood createFood() {
+		return new SnakeFood(randomX(), randomY());
+	}
+	public SnakeHeadEnemy createEnemy() {
+		SnakeHeadEnemy e = new SnakeHeadEnemy(this);
+		e.setPos(randomX(), randomY());
+		return e;
+	}
+	public GameArea(int gameWidth, int gameHeight) {
 		this.addKeyListener(this);
-	}
-	public void setGameDimensions(int w, int h) {
-		gameWidth = w;
-		gameHeight = h;
-	}
-	public GameArea(int width, int height) {
 /*		reset = false;
 		
 		keys = new boolean[5];
@@ -89,25 +107,35 @@ public class GameArea extends JPanel implements KeyListener, Runnable {
 		}
 		FONT = f;
 		
-		setGameDimensions(width, height);
+		this.gameWidth = gameWidth;
+		this.gameHeight = gameHeight;
 		reset();
 		setVisible(true);
 		new Thread(this).start();
 
 	}
-	public void update(Graphics window) {
-		updateCharacters();
-		paint(window);
-	}
 	public void updateCharacters() {
-		int i = 0;
-		ArrayList<GameObject> dead = new ArrayList<GameObject>();
-		Iterator<GameObject> o_i_1 = objects.iterator();
-		while(o_i_1.hasNext()) {
+		for(Iterator<GameObject> i = objects.iterator(); i.hasNext();) {
+			GameObject o = i.next();
+			/*
+			while(o.getPosX() < 0) { o.incPosX(gameWidth); }
+			while(o.getPosX() >= gameWidth) { o.incPosX(-gameWidth); }
+			
+			while(o.getPosY() < 0) { o.incPosY(gameHeight); }
+			while(o.getPosY() >= gameHeight) { o.incPosY(-gameHeight); }
+			*/
+			o.update(timeInterval);
+			if(o.getPosX() < 0 || o.getPosX() >= gameWidth || o.getPosY() < 0 || o.getPosY() >= gameHeight) {
+				o.setActive(false);
+				i.remove();
+			}
+		}
+		
+		/*
+		for(Iterator<GameObject> o_i_1 = objects.iterator(); o_i_1.hasNext();) {
 			GameObject o_1 = o_i_1.next();
 			i++;
-			Iterator<GameObject> o_i_2 = objects.subList(i, objects.size()).iterator();
-			while(o_i_2.hasNext()) {
+			for(Iterator<GameObject> o_i_2 = objects.subList(i, objects.size()).iterator(); o_i_2.hasNext();) {
 				GameObject o_2 = o_i_2.next();
 				o_1.onCollision(o_2);
 				o_2.onCollision(o_1);
@@ -116,47 +144,161 @@ public class GameArea extends JPanel implements KeyListener, Runnable {
 				dead.add(o_1);
 			}
 		}
-		for(GameObject o : dead) {
-			objects.remove(o);
+		*/
+		List<GameObject>[][] grid = new List[gameWidth/16+4][gameHeight/16+4];
+		ArrayList<List<GameObject>> collisions = new ArrayList<List<GameObject>>();
+		for(GameObject o : objects) {
+			int x = 1+o.getPosX()/16, y = 1+o.getPosY()/16;
+			if(grid[x][y] == null) {
+				collisions.add(grid[x][y] = new ArrayList<GameObject>());
+			}
+			grid[x][y].add(o);
 		}
+		for(List<GameObject> tile : collisions) {
+			int n = 1;
+			for(Iterator<GameObject> i1 = tile.iterator(); i1.hasNext(); n++) {
+				GameObject o1 = i1.next();
+				for(Iterator<GameObject> i2 = tile.subList(n, tile.size()).iterator(); i2.hasNext();) {
+					GameObject o2 = i2.next();
+					if(o1.getPos().equals(o2.getPos())) {
+						o1.onCollision(o2);
+						o2.onCollision(o1);
+					}
+				}
+			}
+		}
+		/*
+		for(List<GameObject>[] column : grid) {
+			for(List<GameObject> tile : column) {
+				if(tile != null && tile.size() > 1) {
+					int n = 1;
+					for(Iterator<GameObject> i1 = tile.iterator(); i1.hasNext(); n++) {
+						GameObject o1 = i1.next();
+						for(Iterator<GameObject> i2 = tile.subList(n, tile.size()).iterator(); i2.hasNext();) {
+							GameObject o2 = i2.next();
+							if(o1.getPos().equals(o2.getPos())) {
+								o1.onCollision(o2);
+								o2.onCollision(o1);
+							}
+						}
+					}
+				}
+			}
+		}
+		*/
+		ArrayList<GameObject> dead = new ArrayList<>();
+		objects.removeIf(o -> {
+			boolean result = !o.getActive();
+			if(result) {
+				dead.add(o);
+			}
+			return result;
+		});
+		
+		for(GameObject o : created) {
+			objects.add(o);
+		}
+		created.clear();
 	}
-	public void paint(Graphics window) {
-		window.setColor(Color.WHITE);
-		window.fillRect(0, 0, gameWidth, gameHeight);
+	public void paint(Graphics g) {
+		updateCharacters();
+		
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, gameWidth, gameHeight);
 		
 		timer += 1;
 		//draw score
-		window.setColor(Color.black);
+		g.setColor(Color.black);
 		
-		window.setFont(FONT);
-		window.drawString("Score: " + score,25,50);
+		g.setFont(FONT);
+		int x = 24;
+		int y = 48;
+		g.drawString("Score: " + score, x, 48);
+		y += 8;
+		g.setColor(Color.BLACK);
+		/*
+		g.drawRect(x, y, 240, 15);
+		int pieces = (15 * player.getAbilityTime() / player.getAbilityTimeMax());
+		for(int i = 0; i < pieces; i++) {
+			g.drawImage(SnakeSprites.sprites_player.getBodySprite(BodySprites.BODY), x + i*16, y, null);
+		}
+		*/
+		
+		int barLength = player.getAbilityTimeMax() / player.getMoveInterval();
+		g.drawRect(x, y, 16*barLength, 15);
+		int segments = barLength * player.getAbilityTime() / player.getAbilityTimeMax();
+		for(int i = 0; i < segments; i++) {
+			g.drawImage(SnakeSprites.player.getBodySprite(BodySprites.BODY), x + i*16, y, null);
+		}
+		
+		Iterator<GameObject> o_i = objects.iterator();
+		while(o_i.hasNext()) {
+			GameObject o = o_i.next();
+			o.draw(g);
+		}
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			if(player.directionCheck(Direction.UP)) {
+				player.setDirection(Direction.UP);
+			}
+			break;
+		case KeyEvent.VK_DOWN:
+			if(player.directionCheck(Direction.DOWN)) {
+				player.setDirection(Direction.DOWN);
+			}
+			break;
+		case KeyEvent.VK_RIGHT:
+			if(player.directionCheck(Direction.RIGHT)) {
+				player.setDirection(Direction.RIGHT);
+			}
+			break;
+		case KeyEvent.VK_LEFT:
+			if(player.directionCheck(Direction.LEFT)) {
+				player.setDirection(Direction.LEFT);
+			}
+			break;
+		case KeyEvent.VK_SPACE:
+			player.activateAbility(Ability.RUNNING);
+			break;
+		case KeyEvent.VK_SHIFT:
+			player.activateAbility(Ability.TUNNELLING);
+			break;
+		}
+		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
-
+	public void onFoodEaten(SnakeHead eater, SnakeFood eaten) {
+		objects.add(createFood());
+	}
+	public void create(GameObject o) {
+		created.add(o);
+	}
+	public SnakeHeadPlayer getPlayer() {
+		return player;
+	}
+	public ArrayList<GameObject> getObjects() {
+		return objects;
+	}
 	@Override
 	public void run() {
 		try {
 			while (true) {
-				Thread.currentThread().sleep(timeInterval);
-
+				Thread.sleep(timeInterval);
 				repaint();
 			}
 		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
